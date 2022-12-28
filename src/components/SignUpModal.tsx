00e1676add2a5bfdd13eb6723ffd7e3909640764
Vidit Shah <vidit0210@protonmail.com>
2022-12-28 23:48:06 +0530

    render Fix

--- src/components/SignUpModal.tsx
+++ src/components/SignUpModal.tsx
@@ -12,7 +12,7 @@ export default function SignUpModal({ onSignUpClose }: Props) {
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [birthdate, setBirthdate] = React.useState('dsadsadas');
+  const [birthdate, setBirthdate] = React.useState('');
   const { signUpStep } = useSelector((state: any) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -36,65 +36,7 @@ export default function SignUpModal({ onSignUpClose }: Props) {
     }
   };
 
-  const FirstStepSignUp = () => {
-    return (
-      <>
-        <div className="pl-1 pr-4 py-1 h-12">
-          <button
-            className={`rounded-full bg-blue font-bold text-white mt-2 p-1 pl-3 pr-3 relative right-0 float-right hover:bg-darkblue  + ${
-              !name || !email || !birthdate
-                ? 'opacity-50 cursor-not-allowed'
-                : 'opacity-100 cursor-pointer'
-            }`}
-            onClick={() => dispatch(setSignUpStep(2))}
-          >
-            Next
-          </button>
 
-          <SocialIcon
-            network="twitter"
-            className=" fab fa-twitter text-blue text-2xl "
-          />
-        </div>
-        <div className="pt-5 px-8">
-          <div className="flex justify-between items-center pb-8">
-            <p className="text-2xl font-bold">Create your account</p>
-          </div>
-          <div className="w-full bg-lightblue border-b-2 border-dark mb-8 p-2">
-            <p className="text-dark">Name</p>
-            <input
-              className="w-full bg-lightblue text-lg"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="w-full bg-lightblue border-b-2 border-dark mb-8 p-2">
-            <p className="text-dark">Email</p>
-            <input
-              className="w-full bg-lightblue text-lg"
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <p className="font-bold">Date of birth</p>
-          <p className="text-dark">
-            This will not be shown publicly. Confirm your own age, even if this
-            account is for a business, a pet, or something else.
-          </p>
-          <div className="w-full bg-lightblue border-b-2 border-dark mb-8 p-2">
-            <input
-              className="w-full bg-lightblue text-lg"
-              type="text"
-              value={birthdate}
-              onChange={(e) => setBirthdate(e.target.value)}
-            />
-          </div>
-        </div>
-      </>
-    );
-  };
 
   return (
     <>
@@ -105,7 +47,65 @@ export default function SignUpModal({ onSignUpClose }: Props) {
           onKeyDown={handleEscape}
         ></div>
         <div className="modal-main bg-white w-11/12 max-w-md mx-auto rounded-lg z-50 overflow-y-auto place-self-center max-h-full">
-          {signUpStep === 1 && <FirstStepSignUp />}
+          {signUpStep === 1 && (
+            <>
+              <div className="pl-1 pr-4 py-1 h-12">
+                <button
+                  className={`rounded-full bg-blue font-bold text-white mt-2 p-1 pl-3 pr-3 relative right-0 float-right hover:bg-darkblue  + ${
+                    !name || !email || !birthdate
+                      ? 'opacity-50 cursor-not-allowed'
+                      : 'opacity-100 cursor-pointer'
+                  }`}
+                  onClick={() => dispatch(setSignUpStep(2))}
+                >
+                  Next
+                </button>
+
+                <SocialIcon
+                  network="twitter"
+                  className=" fab fa-twitter text-blue text-2xl "
+                />
+              </div>
+              <div className="pt-5 px-8">
+                <form>
+                  <div className="flex justify-between items-center pb-8">
+                    <p className="text-2xl font-bold">Create your account</p>
+                  </div>
+                  <div className="w-full bg-lightblue border-b-2 border-dark mb-8 p-2">
+                    <label className="text-dark">Name</label>
+                    <input
+                      className="w-full bg-lightblue text-lg"
+                      type="text"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                  <div className="w-full bg-lightblue border-b-2 border-dark mb-8 p-2">
+                    <p className="text-dark">Email</p>
+                    <input
+                      className="w-full bg-lightblue text-lg"
+                      type="text"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <p className="font-bold">Date of birth</p>
+                  <p className="text-dark">
+                    This will not be shown publicly. Confirm your own age, even
+                    if this account is for a business, a pet, or something else.
+                  </p>
+                  <div className="w-full bg-lightblue border-b-2 border-dark mb-8 p-2">
+                    <input
+                      className="w-full bg-lightblue text-lg"
+                      type="text"
+                      value={birthdate}
+                      onChange={(e) => setBirthdate(e.target.value)}
+                    />
+                  </div>
+                </form>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
