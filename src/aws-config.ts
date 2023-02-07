f529b74fe6ec3b27256adcdbc2fc35d215584313
Vidit Shah <vidit0210@protonmail.com>
2023-02-07 18:26:56 +0530

    Config Update

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -1,16 +1,16 @@
 // Configure your AWS resources here
 const awsconfig = {
   Auth: {
-    region: process.env.REACT_APP_REGION,
-    userPoolId: process.env.REACT_APP_USER_POOL_ID,
-    userPoolWebClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID,
-    identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_vZOTCWbsV',
+    userPoolWebClientId: '',
     mandatorySignIn: true,
     storage: window.sessionStorage
   },
-  aws_appsync_region: process.env.REACT_APP_REGION,
+  aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
-  aws_appsync_graphqlEndpoint: process.env.REACT_APP_APPSYNC_API
+  aws_appsync_graphqlEndpoint:
+    'https://hj4awsi6dfgbfmbpltrfcd42ra.appsync-api.us-east-1.amazonaws.com/graphql'
 };
 
 export default awsconfig;
